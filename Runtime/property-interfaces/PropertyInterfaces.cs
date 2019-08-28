184e5cb142cdbb536086b3b595635ae78d3229f0
larry kirschner <larrykirschner@gmail.com>
2019-08-28 15:27:58 -0700

    adds interfaces for IHasTexture and IHasSprite

--- Runtime/property-interfaces/PropertyInterfaces.cs
+++ Runtime/property-interfaces/PropertyInterfaces.cs
@@ -3,111 +3,103 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
 
-namespace BeatThat.Properties{
-	public interface IHasTexture : IHasValue<Texture> {}
+namespace BeatThat.Properties
+{
+    public interface IDrive
+    {
+        object GetDrivenObject();
 
-	public interface IEditsTexture : IHasTexture, IHasValueChangedEvent {}
+        /// <summary>
+        /// Used when loop is detected
+        /// </summary>
+        /// <returns><c>true</c>, if driven was cleared, <c>false</c> otherwise.</returns>
+        bool ClearDriven();
+    }
 
-	public interface IHasMaterial : IHasValue<Material> {}
+    /// <summary>
+    /// A way for a driver to make discoverable that it drives another component, 
+    /// e.g. CurvesFloat implements IDrive<IHasFloat>
+    /// </summary>
+    public interface IDrive<T> : IDrive where T : class
+    {
+        T driven { get; }
+    }
 
-	public interface IHasText : IHasValue<string> {}
+    public interface IEditsBool : IHasBool, IHasValueChangedEvent
+    {
+    }
 
-	public interface IHasColor : IHasValue<Color> {}
+    public interface IEditsTexture : IHasTexture, IHasValueChangedEvent { }
+    public interface IHasBool : IHasValue<bool> { }
 
-	public interface IHasColorBlock : IHasValue<ColorBlock> {}
-		
-	public interface IHasTextInput : IHasText, IHasValueChangedEvent<string>
-	{
-		void ActivateInput();
+    public interface IHasBounds
+    {
+        Bounds bounds { get; }
+    }
 
-		UnityEvent<string> onSubmit { get; }
-	}
-
-	public interface IHasValue
-	{
-		object valueObj { get; }
-	}
-
-	public interface IHasValue<T> : IHasValue
-	{
-		T value { get; set; }
-	}
-
-    public interface IHasProp<T> : IHasValue<T>, IHasValueChangedEvent<T> {}
-
-	public interface IHasFloat : IHasValue<float> {}
-
-	public interface IHasInt : IHasValue<int> {}
-
-	public interface IHasLong : IHasValue<long> {}
-
-	public interface IHasBool : IHasValue<bool> {}
-
-	public interface IHasDateTime : IHasValue<DateTime> {}
-
-	public interface IHasClick 
-	{
-		#if HAS_CLICK_LEGACY
+    public interface IHasClick
+    {
+#if HAS_CLICK_LEGACY
 		bool interactable { get; set; }
 		[Obsolete("use UnityEvent onValueChanged")]event Action Clicked; // TODO: replace with UnityEvent
-		#endif
+#endif
 
-		UnityEvent onClicked { get; }
-	}
+        UnityEvent onClicked { get; }
+    }
 
-	public interface IHasValueObjChanged
-	{
-		UnityEvent onValueObjChanged { get; }
-	}
+    public interface IHasColor : IHasValue<Color> { }
+    public interface IHasColorBlock : IHasValue<ColorBlock> { }
+    public interface IHasDateTime : IHasValue<DateTime> { }
+    public interface IHasFloat : IHasValue<float> { }
+    public interface IHasInt : IHasValue<int> { }
+    public interface IHasLong : IHasValue<long> { }
+    public interface IHasMaterial : IHasValue<Material> { }
+    public interface IHasProp<T> : IHasValue<T>, IHasValueChangedEvent<T> { }
 
-	public interface IHasValueChangedEvent<T>
-	{
-		UnityEvent<T> onValueChanged { get; }
-	}
+    public interface IHasRect
+    {
+        Rect rect { get; }
+    }
 
-	public interface IHasValueChangedEvent 
-	{
-		UnityEvent onValueChanged { get; }
-	}
+    public interface IHasSprite : IHasValue<Sprite> { }
+    public interface IHasText : IHasValue<string> { }
 
-	public interface IHasBounds
-	{
-		Bounds bounds { get; }
-	}
+    public interface IHasTextInput : IHasText, IHasValueChangedEvent<string>
+    {
+        void ActivateInput();
 
-	public interface IHasRect 
-	{
-		Rect rect { get; }
-	}
+        UnityEvent<string> onSubmit { get; }
+    }
 
-	public interface IHasUVRect 
-	{
-		Rect uvRect { get; set; }
-	}
+    public interface IHasTexture : IHasValue<Texture> { }
 
-	public interface IEditsBool : IHasBool, IHasValueChangedEvent
-	{
-	}
+    public interface IHasUVRect
+    {
+        Rect uvRect { get; set; }
+    }
 
+    public interface IHasValue
+    {
+        object valueObj { get; }
+    }
 
-	public interface IDrive 
-	{
-		object GetDrivenObject();
+    public interface IHasValue<T> : IHasValue
+    {
+        T value { get; set; }
+    }
 
-		/// <summary>
-		/// Used when loop is detected
-		/// </summary>
-		/// <returns><c>true</c>, if driven was cleared, <c>false</c> otherwise.</returns>
-		bool ClearDriven();
-	}
+    public interface IHasValueChangedEvent
+    {
+        UnityEvent onValueChanged { get; }
+    }
 
-	/// <summary>
-	/// A way for a driver to make discoverable that it drives another component, 
-	/// e.g. CurvesFloat implements IDrive<IHasFloat>
-	/// </summary>
-	public interface IDrive<T> : IDrive where T : class
-	{
-		T driven { get; }
-	}
+    public interface IHasValueChangedEvent<T>
+    {
+        UnityEvent<T> onValueChanged { get; }
+    }
 
+    public interface IHasValueObjChanged
+    {
+        UnityEvent onValueObjChanged { get; }
+    }
 }
